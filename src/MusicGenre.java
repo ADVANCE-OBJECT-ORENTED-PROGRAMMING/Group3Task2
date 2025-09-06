85ae45c3692519d48b8b11d96c5befdddae42a5f
Bigisaac03 <amanicibembe7@gmail.com>
2025-09-06 23:18:02 +0300

    Commit #1 Branch 2
    in the NV, on line 1 and 2 we imported javax.swing.* and java.awt.* and on line 6 we created a constructor, on line 9 we created a JFrame and from 10-12 we inserted the EXIT ON CLOSE, we set the sizes and set the BorderLayout.

--- src/MusicGenre.java
+++ src/MusicGenre.java
@@ -1,3 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class MusicGenre {
-    
+    //constructor creation
+    public MusicGenre(){
+
+        //JFrame creation
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,400);
+        frame.setLayout(new BorderLayout());
+    }
 }
