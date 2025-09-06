8bbdf4de6f3fa0f55277ebd91fddbb26a1d0a0c2
Bigisaac03 <amanicibembe7@gmail.com>
2025-09-06 23:46:51 +0300

    Commit #1 Branch 3
    In the NV, on line 20-21, we created the Rap Music button and put it in the south, on line 24-25, we created the Gospel Music button and put it in the west, on line 28-29, we created the Ballads Music button and put it in the east and on line 32, we set Visible to true.

--- src/MusicGenre.java
+++ src/MusicGenre.java
@@ -15,5 +15,20 @@ public class MusicGenre {
         JLabel titleLabel = new JLabel("My Music Genre",SwingConstants.CENTER);
         titleLabel.setFont(new Font("Arial",Font.BOLD,25));
         frame.add(titleLabel, BorderLayout.NORTH);
+
+        //Rap Music Button creation
+        JButton rapMusic = new JButton("Rap Music");
+        frame.add(rapMusic, BorderLayout.SOUTH);
+
+        //Gospel Music Creation
+        JButton gospelMusic = new JButton("Gospel Music");
+        frame.add(gospelMusic, BorderLayout.WEST);
+
+        //Ballads Music Button Creation
+        JButton balladsMusic = new JButton("Ballads Music");
+        frame.add(balladsMusic,BorderLayout.EAST);
+
+        //setting the visible to true
+        frame.setVisible(true);
     }
 }
