1a28ea734f288ae2ee6d2d49b974c13ed9425e1f
Bigisaac03 <amanicibembe7@gmail.com>
2025-09-06 23:30:18 +0300

    Commit #2 Branch 2
    In the NV, on line line 15, we created JLabel, add the title and center it, on line 16 and 17 we set the title in bold and put in the north.

--- src/MusicGenre.java
+++ src/MusicGenre.java
@@ -10,5 +10,10 @@ public class MusicGenre {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,400);
         frame.setLayout(new BorderLayout());
+
+        //JLabel creation
+        JLabel titleLabel = new JLabel("My Music Genre",SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial",Font.BOLD,25));
+        frame.add(titleLabel, BorderLayout.NORTH);
     }
 }
